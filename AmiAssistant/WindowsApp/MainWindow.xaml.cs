d342a1c724032f05adeba9c361c9b4801bd86047
fan1ech00 <fan1ech000@tuta.io>
2021-12-17 06:06:22 +0200

    upd

--- AmiAssistant/WindowsApp/MainWindow.xaml.cs
+++ AmiAssistant/WindowsApp/MainWindow.xaml.cs
@@ -98,21 +98,62 @@ namespace WindowsApp
             }
         }
 
+        private static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
+        {
+            // Get the subdirectories for the specified directory.
+            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+
+            if (!dir.Exists)
+            {
+                throw new DirectoryNotFoundException(
+                    "Source directory does not exist or could not be found: "
+                    + sourceDirName);
+            }
+
+            DirectoryInfo[] dirs = dir.GetDirectories();
+
+            // If the destination directory doesn't exist, create it.       
+            Directory.CreateDirectory(destDirName);
+
+            // Get the files in the directory and copy them to the new location.
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                string tempPath = System.IO.Path.Combine(destDirName, file.Name);
+                file.CopyTo(tempPath, false);
+            }
+
+            // If copying subdirectories, copy them and their contents to new location.
+            if (copySubDirs)
+            {
+                foreach (DirectoryInfo subdir in dirs)
+                {
+                    string tempPath = System.IO.Path.Combine(destDirName, subdir.Name);
+                    DirectoryCopy(subdir.FullName, tempPath, copySubDirs);
+                }
+            }
+        }
+
         public bool SetAutorunValue(bool autorun)
         {
             const string name = "AmiAssistant";
+            const string nameExe = "WindowsApp.exe";
             string ExePathSource = System.Windows.Forms.Application.StartupPath;
-            var ExeDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) 
+            var ExeDirectory = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) 
                 + System.IO.Path.DirectorySeparatorChar + name + System.IO.Path.DirectorySeparatorChar;
             
-            if (System.Windows.Forms.Application.ExecutablePath == ExeDirectory)
-                return false;
 
-            if (!Directory.Exists(ExeDirectory + "AmiAssistant"))
+            if (System.Windows.Forms.Application.ExecutablePath == ExeDirectory + nameExe)
+            {
+                return true;
+            }
+                
+            if (!Directory.Exists(ExeDirectory + name))
             {
                 Directory.CreateDirectory(ExeDirectory);
             }
 
+            //DirectoryCopy(ExePathSource, ExeDirectory, true);
             CopyFilesRecursively(ExePathSource, ExeDirectory);
 
             RegistryKey reg;
@@ -121,14 +162,14 @@ namespace WindowsApp
             {
                 if (autorun)
                 {
-                    reg.SetValue(name, ExeDirectory + "WindowsApp.exe");
+                    reg.SetValue(name, ExeDirectory + nameExe);
 
                     // create shortcut
                     WshShell shell = new WshShell();
                     string shortcutPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\AmiAssistant.lnk";
                     IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(shortcutPath);
                     shortcut.Description = "Ярлык для AmiAssistant";
-                    shortcut.TargetPath = ExeDirectory + "WindowsApp.exe";
+                    shortcut.TargetPath = ExeDirectory + nameExe;
                     shortcut.Save();
                 }
                 else
